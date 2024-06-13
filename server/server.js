bdf68e2bacf90788a1efae4aa90f47020c493fe0
FITTEROYSTER <ARYANSUDHEER03@GMAIL.COM>
2024-06-13 10:40:54 +0530

    commit "13/06/2024"

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
+import User from "./Schema/User.js";
+import { nanoid } from "nanoid";
 
 const server = express();
 let PORT = 3000;
@@ -14,6 +16,28 @@ server.use(express.json());
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
+
+const formatDatatoSend = (user) => {
+  return {
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
+
+//Checks for username in DB to see if exists.
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+
+  //Looks throught DB to see if username exists, if not, it return the first part of the email
+  let isUsernameNotUniqe = await User.exists({
+    "personal_info.username": username,
+  }).then((result) => result);
+
+  isUsernameNotUniqe ? (username += nanoid().substring(0, 5)) : "";
+
+  return username;
+};
 
 server.post("/signup", (req, res) => {
   let { fullname, email, password } = req.body;
@@ -36,8 +60,25 @@ server.post("/signup", (req, res) => {
         "Password should be 6-20 letter, must contain a number, an uppercase, lowercase and special character.",
     });
   }
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername(email);
+    let user = new User({
+      personal_info: { fullname, email, password: hashed_password, username },
+    });
 
-  return res.status(200).json({ status: "okay" });
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json(formatDatatoSend(u));
+      })
+
+      .catch((err) => {
+        if (err.code == 11000) {
+          return res.status(500).json({ error: "Email elaready exists" });
+        }
+        return res.status(500).json({ error: err.message });
+      });
+  });
 });
 
 server.listen(PORT, () => {
