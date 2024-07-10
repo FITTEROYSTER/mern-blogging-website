bacab389a1f74e06c61f9f9981c959f43281df9f
FITTEROYSTER <ARYANSUDHEER03@GMAIL.COM>
2024-07-10 19:01:19 +0530

    Update editor.pages.jsx

--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -3,14 +3,18 @@ import { Navigate } from "react-router-dom";
 import { UserContext } from "../App";
 
 const Editor = () => {
+  const [editorState, seEditorState] = useState("editor");
+
   let {
     userAuth: { access_token },
   } = useContext(UserContext);
 
   return access_token == null ? (
     <Navigate to="/signin" />
+  ) : editorState == "editor" ? (
+    <h1>Blog Editor</h1>
   ) : (
-    <h1>You can access</h1>
+    <h1>Publish form</h1>
   );
 };
 
